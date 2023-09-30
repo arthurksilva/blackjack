ce7c9e0acecf698847ab43ea86c0f05f1baa72ac
Arthur Carvalho da Silva <arthursilvainformatica@gmail.com>
2023-09-30 19:58:39 -0300

    Matriz dos Cards

--- blackjack.py
+++ blackjack.py
@@ -3,15 +3,37 @@ import random
 def cards():
     print()
 
-tipos_cards = [2,3,4,5,6,7,8,9,10]
+cards_typs = [
+    ["Card 2", 2],
+    ["Card 3", 3],
+    ["Card 4", 4],
+    ["Card 5", 5],
+    ["Card 6", 6],
+    ["Card 7", 7],
+    ["Card 8", 8],
+    ["Card 9", 9],
+    ["Card 10", 10],
+    ["Valete", 10],
+    ["Dama", 10],
+    ["Rei", 10]
+]
 
-opecao = input("Você Dejesar jogo BLACKJACK? S/N").lower()
 
-if opecao == "s":
+
+
+
+
+
+
+
+
+"""opecao = input("Você Dejesar jogo BLACKJACK? S/N").lower()
+
+if opecao == "s",
     cont = 1
-else:
+else,
     cont = 0
 
-while(cont == 1):
-    print()
+while(cont == 1),
+    print()"""
 
