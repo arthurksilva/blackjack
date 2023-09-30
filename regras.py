afc1eecac0b986e114c1ad690d71d69b763663de
Arthur Carvalho da Silva <arthursilvainformatica@gmail.com>
2023-09-30 19:35:05 -0300

    regras do jogo

---
+++ regras.py
@@ -0,0 +1,39 @@
+"""O Blackjack é um jogo de cartas muito popular em cassinos ao redor do mundo. Também é conhecido como "21" devido ao objetivo principal do jogo, que é obter uma mão cujo valor seja igual ou o mais próximo possível de 21, sem ultrapassar esse valor. Abaixo, vou explicar o passo a passo de como funciona o Blackjack:
+
+1. **Objetivo do Jogo**:
+   - O objetivo principal do Blackjack é vencer o dealer (a casa) obtendo uma mão com um valor total de cartas mais próximo possível de 21, sem exceder esse valor.
+
+2. **Valores das Cartas**:
+   - As cartas numéricas (2 a 10) têm o valor nominal delas.
+   - As cartas faciais (Valete, Dama, Rei) valem cada uma 10 pontos.
+   - O Ás (As) pode valer 1 ou 11 pontos, dependendo da escolha do jogador. Isso permite flexibilidade na construção das mãos.
+
+3. **Distribuição Inicial de Cartas**:
+   - Cada jogador recebe duas cartas viradas para cima.
+   - O dealer também recebe duas cartas, uma virada para cima (carta "aberta") e uma virada para baixo (carta "escondida").
+
+4. **Opções do Jogador**:
+   - Após receber suas duas cartas iniciais, um jogador tem as seguintes opções:
+     a. **Hit (Pedir Carta)**: Pedir uma carta adicional para tentar chegar mais perto de 21.
+     b. **Stand (Parar)**: Manter sua mão atual e passar a vez para o próximo jogador.
+     c. **Double Down (Dobrar)**: Dobrar a aposta inicial e receber apenas mais uma carta.
+     d. **Split (Dividir)**: Se as duas cartas iniciais forem do mesmo valor, o jogador pode dividir a mão em duas e continuar jogando com duas mãos separadas.
+
+5. **Pontuação e Vitória**:
+   - Se a mão do jogador excede 21 pontos, ele perde automaticamente (chamado de "estouro" ou "bust").
+   - Se o jogador alcança exatamente 21 pontos com suas duas primeiras cartas (um Ás e um 10 ou uma carta facial), isso é chamado de "Blackjack" e geralmente paga um bônus.
+   - Se o jogador opta por parar (stand), o dealer revela sua carta escondida.
+   - O dealer deve sempre bater (pedir carta) até ter 17 pontos ou mais. Após atingir 17 pontos ou mais, o dealer deve parar.
+   - O jogador vence se tiver uma mão que tenha um valor mais alto do que o do dealer, sem estourar.
+   - Se o jogador e o dealer empatam com a mesma pontuação, é chamado de "push", e o jogador mantém sua aposta, mas não ganha nada adicional.
+
+6. **Pagamento de Apostas**:
+   - Se o jogador vencer, ele geralmente recebe uma aposta igual em retorno.
+   - Se o jogador obtiver um Blackjack, geralmente é pago 3:2 ou 2:1, dependendo das regras do cassino.
+   - Se o dealer vencer, o jogador perde sua aposta.
+   - Se ocorrer um empate (push), o jogador mantém sua aposta original.
+
+7. **Regras Especiais**:
+   - As regras podem variar de um cassino para outro, incluindo regras sobre divisão de cartas, rendição antecipada e seguro contra Blackjack do dealer.
+
+Lembre-se de que as regras específicas do Blackjack podem variar, portanto, é importante verificar as regras da mesa em que você está jogando antes de começar. O Blackjack é um jogo de habilidade e estratégia, e entender quando pedir cartas, parar ou dividir é fundamental para maximizar suas chances de sucesso."""
