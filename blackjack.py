8a891947d902fc92ce713387fefcde717de8af73
Arthur Carvalho da Silva <arthursilvainformatica@gmail.com>
2023-09-30 19:31:21 -0300

    Iniciando o Jogo

---
+++ blackjack.py
@@ -0,0 +1,17 @@
+import random
+
+def cards():
+    print()
+
+tipos_cards = [2,3,4,5,6,7,8,9,10]
+
+opecao = input("Você Dejesar jogo BLACKJACK? S/N").lower()
+
+if opecao == "s":
+    cont = 1
+else:
+    cont = 0
+
+while(cont == 1):
+    print()
+
